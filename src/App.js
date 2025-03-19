fdf6594550b66924a2c13e79c19754d710e7a7b7
zahnx <breadsw01@gmail.com>
2025-03-19 17:30:33 +0800

    Added an event listener for Escape key to close the movie details

--- src/App.js
+++ src/App.js
@@ -115,9 +115,10 @@ export default function App() {
           setMovies(data.Search);
           setError("");
         } catch (err) {
-          console.error(err.message);
-
-          if (err.name !== "AbortError") setError(err.message);
+          if (err.name !== "AbortError") {
+            console.log(err.message);
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -128,6 +129,8 @@ export default function App() {
         setError("");
         return;
       }
+
+      handleCloseMovie();
       fetchMovies();
 
       return function () {
@@ -329,6 +332,21 @@ function MovieDetais({ selectedId, onCloseMovie, onWatched, watched }) {
     onWatched(newWatchedMovie);
     onCloseMovie(true);
   }
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code === "Escape") {
+          onCloseMovie();
+        }
+      }
+      document.addEventListener("keydown", callback);
+
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [onCloseMovie]
+  );
 
   useEffect(
     function () {
