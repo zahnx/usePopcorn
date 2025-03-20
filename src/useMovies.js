a91473eb01e8a31de2bcc3997b6295466eb8e04c
zahnx <breadsw01@gmail.com>
2025-03-20 17:10:49 +0800

    For learning purposes, I created a custom hook useLocalStorageState

--- src/useMovies.js
+++ src/useMovies.js
@@ -1,14 +1,13 @@
 import { useEffect, useState } from "react";
 const KEY = "b89dc3d9";
 
-export function useMovies(query, callback) {
+export function useMovies(query) {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
 
   useEffect(
     function () {
-      callback?.();
       const controller = new AbortController();
       async function fetchMovies() {
         try {
